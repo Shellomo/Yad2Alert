7f67eeca643533a0bcd88684ddd09d3ff788fc8e
Shellomo <155258678+Shellomo@users.noreply.github.com>
2024-03-15 11:41:43 +0200

    update s3_db_files.py

--- s3_db_files.py
+++ s3_db_files.py
@@ -1,27 +1,23 @@
 import json
-
+import os
 import boto3
 from botocore import exceptions
-# import settings
-import settings_temp as settings  # ToDo: remove this
+
+bucket_name = os.getenv('BUCKETNAME')
 
 
 def get_db_file(file_name, create=True):
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=settings.AWS_ACCESS_KEY,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
-    )
+    s3 = boto3.client('s3')
     # make sure bucket exists
     try:
-        s3.head_bucket(Bucket=settings.BUCKET_NAME)
-    except exceptions.ClientError:
-        print('Bucket does not exist.\nExiting...')
+        s3.head_bucket(Bucket=bucket_name)
+    except exceptions.ClientError as e:
+        print(e)
         exit(1)
 
     # make sure file exists
     try:
-        s3.head_object(Bucket=settings.BUCKET_NAME, Key=file_name)
+        s3.head_object(Bucket=bucket_name, Key=file_name)
     except exceptions.ClientError:
         if create:
             print('File does not exist. creating...')
@@ -32,19 +28,15 @@ def get_db_file(file_name, create=True):
             print(f'File {file_name} does not exist. exiting...')
             exit(1)
 
-    s3.download_file(settings.BUCKET_NAME, file_name, file_name)
+    s3.download_file(bucket_name, file_name, file_name)
 
     with open(file_name, 'r', encoding='utf8') as f:
         return json.load(f)
 
 
 def put_db_file(file_name):
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=settings.AWS_ACCESS_KEY,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
-    )
+    s3 = boto3.client('s3')
     print('uploading file: ' + file_name)
     with open(file_name, 'r', encoding='utf8') as f:
         print(json.load(f))
-    s3.upload_file(file_name, settings.BUCKET_NAME, file_name)
+    s3.upload_file(file_name, bucket_name, file_name)
